3cd74f5b9bae529e2b2bd175c6514ff2510ed72e
Ярослав Нестеров <atomofiron@iWTF.local>
2024-07-11 23:27:44 +0200

    rename files and fix the build script

--- app/build2.gradle.kts
+++ app/build2.gradle.kts
@@ -1,64 +1,58 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("kapt")
 }
 
 android {
-    compileSdk = 33
-    val appId = "ru.raslav.wirelessscan"
+    val packageName = "ru.raslav.wirelessscan"
+    namespace = packageName
+    compileSdk = 34
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     defaultConfig {
-        applicationId = appId
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 39
-        versionName = "3.0.0"
+        versionName = "2.1.0"
     }
-
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     buildTypes {
+        val fileProvider = ".FileProvider"
         getByName("debug") {
             applicationIdSuffix = ".debug"
+            val providerAuthority = packageName + applicationIdSuffix + fileProvider
+            manifestPlaceholders["PROVIDER"] = providerAuthority
+            buildConfigField("String", "AUTHORITY", "\"$providerAuthority\"")
         }
         getByName("release") {
             isMinifyEnabled = true
+            val providerAuthority = packageName + fileProvider
+            manifestPlaceholders["PROVIDER"] = providerAuthority
+            buildConfigField("String", "AUTHORITY", "\"$providerAuthority\"")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    namespace = appId
 }
 
 dependencies {
-    //noinspection DifferentStdlibGradleVersion
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.21")
-
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-
-    implementation("com.google.android.material:material:1.9.0")
-
-    implementation("com.google.dagger:dagger:2.46")
-    kapt("com.google.dagger:dagger-compiler:2.46")
-    kapt("com.google.dagger:dagger-android-processor:2.39.1")
-
-    implementation("com.github.atomofiron:android-window-insets-compat:1.1.1")
+    implementation("io.github.atomofiron:extended-insets:2.0.0-rc0")
     implementation("org.simpleframework:simple-xml:2.7.1") {
         exclude("stax", "stax")
         exclude("stax-api", "stax-api")
