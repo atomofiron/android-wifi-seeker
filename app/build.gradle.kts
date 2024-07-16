bb894239f3ea64918ae225642b8eb76d4d974798
atomofiron <atomofiron@gmail.com>
2024-07-16 11:05:15 +0200

    up version code to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 34
-        versionCode = 15
+        versionCode = 17
         versionName = "2.1.0"
     }
     compileOptions {
